9068b3710277271e23a5a9925f532836d2b3bb7e
ghong25 <55066232+ghong25@users.noreply.github.com>
2020-03-03 18:07:14 -0500

    remove config

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-mystery'
-    SQLALCHEMY_DATABASE_URI = "postgres://postgres:zeKaXHNnj3FpnGJj@database-1.c5aqdxmdgobi.us-east-1.rds.amazonaws.com:5432/postgres"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
